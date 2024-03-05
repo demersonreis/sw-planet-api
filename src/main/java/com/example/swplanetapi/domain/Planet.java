75f112b4b9ccf67dfd8c869bc807da72ea188351
demersonreis <demersonsilvadosreis@gmail.com>
2024-03-05 20:29:18 -0300

    first commit

---
+++ src/main/java/com/example/swplanetapi/domain/Planet.java
@@ -0,0 +1,36 @@
+package com.example.swplanetapi.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "planets")
+public class Planet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false )
+    private Long id;
+    private String name;
+    private String climate;
+    private String terrain;
+
+    public Planet() {
+    };
+
+    public Planet(Long id, String name, String climate, String terrain) {
+        this.id = id;
+        this.name = name;
+        this.climate = climate;
+        this.terrain = terrain;
+    }
+
+
+}
